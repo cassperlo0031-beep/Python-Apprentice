3192d64d6600355a3c09d4ea23c5a2701bb5bc32
cassperlo0031-beep <cassperlo0031@gmail.com>
2026-03-12 02:19:44 +0000

    looping

--- lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
+++ lessons/10_Turtles/40_Loops/30_Loop_with_Turtle.py
@@ -1,1 +1,9 @@
 
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(600,600,0,0)               # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina                          # Turn tina left by the left turn
+for i in range(6):                       # Loop 4 times for the 4 sides of the square
+    tina.forward(150)                    # Move tina forward by 150 pixels
+    tina.left(60)
+turtle.exitonclick()
